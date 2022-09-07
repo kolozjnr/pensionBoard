2774253541e09c7707aa5faf1f772ec90653e661
Barnabas KolozJnr <67397553+kolozjnr@users.noreply.github.com>
2022-09-07 09:52:08 +0100

    initial commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+      /**
+     * uuid setup
+     * 
+     */
+    public $incrementing = false;
+
+    protected $keyType = 'uuid';
+
 }
